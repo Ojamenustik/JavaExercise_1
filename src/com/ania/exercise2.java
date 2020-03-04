8fcdc8b017466c536bc981b9eb8ec27b87c3811f
Ojamenustik <ania.gajda96@gmail.com>
2020-03-04 14:39:42 +0100

    getters, setters and this

--- src/com/ania/exercise2.java
+++ src/com/ania/exercise2.java
@@ -12,7 +12,27 @@ class Robot{
     public void move(String direction, double distance){
         System.out.println("Moving "+ distance + " meters in direction " + direction);
     }
+}
 
+class Frog{
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
 }
 
 public class exercise2 {
@@ -26,7 +46,16 @@ public class exercise2 {
         sam.move("West", 12.5);
 
         String greeting = "Hello there <3";
-
         sam.speak(greeting);
+
+        ///////Frog
+        Frog froggy = new Frog();
+
+        froggy.setName("Bertie");
+        froggy.setAge(1);
+
+        System.out.println(froggy.getName());
+
+
     }
 }
